1518d22657b58c01cbc6c8d30089e0bb06f8c207
dennyclain123 <dennyclain123@gmail.com>
2022-08-21 00:24:17 +0630

    My Web

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AKM CourseWork</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Try My Recipe</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#history">History</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#recipe">Recipes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">Login</a>
+        </li>
+      </ul>
+  <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<div class="content">
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://img.wallscloud.net/converted/956464037-zhareniy-steyk-iz-govyadini-gXy6-1366x768-MM-100.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Seasonal Food by month</h5>
+        <p>Make August meals al fresco. Start with delicate scallops.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.zastavki.com/pictures/1366x768/2017Food_Fresh_products_for_a_hamburger_on_a_gray_background_120383_24.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Food around the world</h5>
+        <p>We love Greek food - from a simple Greek salad, to grilled Greek lamb skewers to a classic moussaka.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://s1.1zoom.me/b5050/794/Cheese_Pasta_Plate_571950_1366x768.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Food occasions within the year</h5>
+        <p>If you're getting the gang together for a party full of sunshine and good food, we've got the recipes for you.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+<center>
+<div class="about" id="about">
+    <h3>Who We Are</h3>
+    <br>
+    <p> Simply Recipes is here to help you cook delicious meals with less stress and more joy. We offer recipes and cooking advice for home cooks, by home cooks. Helping create “kitchen wins” is what we’re all about.</p>
+    <p> Simply Recipes was founded in 2003 by Elise Bauer as a home cooking blog to record her favorite family recipes. Today, Simply Recipes has grown into a trusted resource for home cooks with more than 3,000 tested recipes, guides, and meal plans, drawing over 15 million readers each month from around the world. We’re supported by a diverse group of recipe developers, food writers, recipe and product testers, photographers, and other creative professionals.</p>
+  </div>
+</center>
+<center>
+<div class="about" id="history">
+    <h3>Our History</h3>
+    <br>
+    <p>Elise Bauer was a busy Silicon Valley executive when she became sick with a flu that wouldn’t go away in 2001. In 2003, Elise moved home with her parents and lived with them for several years, recovering from chronic fatigue and documenting her parents’ cooking on Simply Recipes. (Here’s more of that story.) Over the years, what started as a small blog grew to reach millions of readers every month! In 2020, Simply Recipes was acquired by Dotdash Meredith, a New York-based media company.</p>
+  </div>
+</center>
+<div class="header" id="recipe">
+    <center><h3>TYPES OF RECIPES</h3></center>
+</div>
+<div class="container">
+  <div class="row justify-content-center mt-4">
+    <div class="col-sm-3 m-2">
+        <div class="card">
+        <img src="https://www.kikkoman.eu/fileadmin/_processed_/4/b/csm_Black_Pepper_Beef_4qqq_745803d7ff.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Beefs</h5>
+                <p class="card-text">These steak bites are perfect for your steakhouse cravings.</p>
+                <a href="#" class="btn btn-primary">Collection</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3 m-2">
+    <div class="card">
+            <img src="https://images.lifestyleasia.com/wp-content/uploads/sites/6/2022/05/02111120/Chicken-recipe-1600x900.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Chickens</h5>
+                <p class="card-text">These chicken recipes will be your new favourite homecooked meal.</p>
+                <a href="#" class="btn btn-primary">Collection</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3 m-2">
+    <div class="card">
+            <img src="https://media.npr.org/assets/img/2014/09/16/201204281118201391908-2-_wide-f91bb7d396eb5b6432adb38c0c050145f3a64aa4.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Porks</h5>
+                <p class="card-text">The Secret To This Melt-In-Your-Mouth Pork Is in pork collections</p>
+                <a href="#" class="btn btn-primary">Collection</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3 m-2">
+    <div class="card">
+            <img src="https://cdn.cnn.com/cnnnext/dam/assets/220530155739-05-a-classic-french-dishes-crpes-suzette-full-169.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Cuisines</h5>
+                <p class="card-text">Hey, there is a cuisine collection that you like to watch.</p>
+                <a href="#" class="btn btn-primary">Collection</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3 m-2">
+    <div class="card">
+            <img src="https://static01.nyt.com/images/2020/04/14/dining/sc-stirfry/sc-stirfry-videoSixteenByNineJumbo1600.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Vegies</h5>
+                <p class="card-text">Dont worry, we don't forget about the vegetarians.</p>
+                <a href="#" class="btn btn-primary">Collection</a>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+<footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
+    <div class="container text-center">
+      <small>Copyright &copy; Aung Khant Moe</small>
+    </div>
+  </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AKM CourseWork</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Try My Recipe</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">History</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Recipes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">Login</a>
+        </li>
+      </ul>
+  <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<div class="mt-4">
+<center><h3>Create New Account</h3></center>
+</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+        <form>
+        <div class="mb-3 mt-4">
+            <label for="exampleInputEmail1" class="form-label">Full Name</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Username</label>
+            <input type="text" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button type="submit" class="btn btn-success">Login</button>
+        <p class="mt-2">Already have an account? <a href="register.php">Login here!</a></p>
+        </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
